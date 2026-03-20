132cdc258d1919b1c9e15122977f9794691e48f0
MERTULAS <h.mert.ulas@gmail.com>
2026-03-21 01:56:28 +0300

    implement affine matrices for 3d transformations

--- playground.js
+++ playground.js
@@ -1,4 +1,4 @@
-import { Box, initCanvas, Rectangle, Polygon, Triangle, ImageShape, Pixel, Point3D, Vector, Matrix } from "./lib/index.js";
+import { Box, initCanvas, Rectangle, Polygon, Triangle, ImageShape, Pixel, Point3D, Vector, Matrix, Group } from "./lib/index.js";
 
 const { ctx, clearCanvas } = initCanvas("canvas1");
 
@@ -15,64 +15,74 @@ const triangle2 = new Triangle([600, 500], [700, 200], [500, 600]);
 const triangle3 = new Triangle([600, 500], [700, 200], [500, 600]);
 const polygon1 = new Polygon([300, 300], [400, 150], [400, 300], [350, 350], [300, 400], [250, 200]);
 
+const group1 = new Group(rect1, rect1copy, rect2, triangle1, triangle2, triangle3, polygon1);
 
-const image = new Image();
-image.crossOrigin = "anonymous";
-image.src = "./assets/road.png";
+const tetrisRect1 = new Rectangle(100, 100, 50, 50);
+const tetrisRect2 = new Rectangle(150, 100, 50, 50);
+const tetrisRect3 = new Rectangle(200, 100, 50, 50);
+const tetrisRect4 = new Rectangle(250, 100, 50, 50);
 
-const box1 = new Box([350, 300, 0], 100);
 
-image.onload = () => {
-  const w = image.naturalWidth;
-  const h = image.naturalHeight;
+const tetrisRect5 = new Rectangle(100, 100, 50, 50);
+const tetrisRect6 = new Rectangle(150, 100, 50, 50);
+const tetrisRect7 = new Rectangle(200, 100, 50, 50);
+const tetrisRect8 = new Rectangle(150, 50, 50, 50);
 
-  ctx.drawImage(image, 100, 100, w, h);
+const group2 = new Group(tetrisRect1, tetrisRect2, tetrisRect3, tetrisRect4);
+const group3 = new Group(tetrisRect5, tetrisRect6, tetrisRect7, tetrisRect8);
 
-  const imageData = ctx.getImageData(100, 100, w, h);
-  const imageDataCopy = ctx.getImageData(100, 100, w, h);
+group3.translate(100, 100);
 
-  const imageShape = new ImageShape(0, 0, imageData.data, w, h);
-  const imageShapeCopy = new ImageShape(0, 0, imageDataCopy.data, w, h);
-  imageShape.translate(350, 100);
-  imageShapeCopy.translate(350, 370);
-  imageShape.scale(-1, 1);
+const vec = new Vector();
 
-  imageShape.shearX(2);
-  imageShapeCopy.shearXY(-.1, .1);
-  imageShapeCopy.rotate(-32);
-    imageShapeCopy.scale(-1.1, -1.1);
+console.log(Vector.normalize([1, 1, 1]));
 
-  box1.rotateX(1);
-  box1.draw();
-  imageShape.draw();
 
-  const animate = () => {
+const box1 = new Box(400);
+console.log('Box1 Center:', box1.center);
+console.log('Box1 Points:', box1.points);
+box1.translate3D(300, 300, 0);
+console.log('Box1 Center after translation:', box1.center);
+console.log('Box1 Points after translation:', box1.points);
+
+console.log(box1.points);
+
+addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") {
+        box1.translate3D(0, 10, 0);
+        //group2.translate(0, 10);
+        //console.log(box1.points);
+    }
+    if (e.key === "ArrowDown") {
+        box1.translate3D(0, -10, 0);
+        //group2.translate(0, -10);
+    }
+    if (e.key === "ArrowLeft") {
+        box1.translate3D(-10, 0, 0);
+        //group2.translate(-10, 0);
+    }
+    if (e.key === "ArrowRight") {
+        box1.translate3D(10, 0, 0);
+        //group2.translate(10, 0);
+    }
+});
+
+const animate = () => {
     clearCanvas();
+
+    //group1.draw();
+    //group1.rotate(1);
+    //group2.draw();
+    //group2.rotate(45);
+
+    //group3.draw();
     
-    imageShape.rotate(1);
-    box1.rotateX(1);
-    box1.rotateY(-1);
-    box1.rotateZ(-2);
-    imageShape.draw();
-    imageShapeCopy.draw();
+    box1.rotateX(.5);
+    box1.rotateY(.5);
+    box1.rotateZ(.5);
+
     box1.draw();
 
     requestAnimationFrame(animate);
   }
   animate();
-
-  addEventListener("keydown", (e) => {
-    if (e.key === "ArrowUp") {
-        imageShape.translate(0, 10);
-    }
-    if (e.key === "ArrowDown") {
-        imageShape.translate(0, -10);
-    }
-    if (e.key === "ArrowLeft") {
-        imageShape.translate(10, 0);
-    }
-    if (e.key === "ArrowRight") {
-        imageShape.translate(-10, 0);
-    }
-});
-};
